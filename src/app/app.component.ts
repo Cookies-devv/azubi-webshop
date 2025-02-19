ed01fc99e500740e7cfb7885e4f50d5963a493f4
Cookies-devv <janic.breisinger@gmail.com>
2025-02-19 14:31:44 +0100

    Styling

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -6,47 +6,40 @@ import { WarenkorbService } from './services/warenkorb/warenkorb.service';
 import {CommonModule, JsonPipe, NgFor, NgForOf} from '@angular/common';
 import { Produkt } from './product-card/product_interface';
 import { WarenkorbComponent } from "./warenkorb/warenkorb.component";
+import { ProfilePopupComponent } from "./profile-popup/profile-popup.component";
 
 
 @Component({
   selector: 'app-root',
-  imports: [ProductCardComponent, WarenkorbComponent, CommonModule],
+  imports: [ProductCardComponent, WarenkorbComponent, CommonModule, ProfilePopupComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.scss'],  
 })
+
 export class AppComponent implements OnInit {
+[x: string]: any;
 
   title = 'azubi-webshop';
   products: Produkt[] = productMock;
   warenkorbProduct: Produkt[] = [];
  warenkorbService: WarenkorbService = inject(WarenkorbService);
- isWarenkorbVisible = false;
+ isWarenkorbVisible = true;
  isExpand = false;
 
   ngOnInit(): void {
     this.warenkorbProduct = this.warenkorbService.warenkorb;
     this.isExpand = false;
   }
-
-  profilClicked() {
-    alert('Profil');
-  }
-
-  einkaufeClicked() {
-    alert('Einkaufen Fenster');
-  }
-
-  logoutClicked() {
-    alert('Logout Fenster');
-  }
-
-  toggleWarenkorbVisibility() {
-    this.isWarenkorbVisible = !this.isWarenkorbVisible;
+  
+  warenkorbSchliessen(){
+    this.isExpand = false;
+    this.isWarenkorbVisible = true;
   }
 
   toggleWarenkorb() {
     this.isExpand = !this.isExpand;
     console.log("Warenkorb Status:", this.isExpand);
+    this.isWarenkorbVisible = !this.isWarenkorbVisible;
   }
   
 }
